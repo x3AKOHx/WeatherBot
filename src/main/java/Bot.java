c4cc4f56ef1ab38cf9e2778164552371471ab157
Zoom90x <zoom90x@gmail.com>
2022-02-09 11:02:56 +0300

    Initial commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,37 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+    final String BOT_NAME = "SaushWeatherBot";
+    final String BOT_TOKEN = "5267180843:AAGAQfSJhoujYjqoC-kUFan-qkKuuBCNJ08";
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(chatId));
+            message.setText(messageText);
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/WeatherInfo.java
@@ -0,0 +1,3 @@
+public class WeatherInfo {
+    public static final String KEY = "4e31d834f817a57f1cb6980cf70ce319";
+}
